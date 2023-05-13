2ef94bac0ef09f13289e3730bdee7b00b9c2e43e
phatlino <phatlongtoan@gmail.com>
2023-05-13 20:00:40 +0700

    update audit pick user 2

--- src/BLL/ExecuteData.java
+++ src/BLL/ExecuteData.java
@@ -9,7 +9,7 @@ public class ExecuteData {
     }
     public static boolean CreateAuditPolicy(String objectSchema, String objectName, String policyName, String statement)
     {
-        String temp = String.format("BEGIN DBMS_FGA.ADD_POLICY(object_schema      => '%s',object_name        => '%s',policy_name        => '%s',statement_types    => '%s');END;commit work;",objectSchema, objectName, policyName, statement);
+        String temp = String.format("BEGIN DBMS_FGA.ADD_POLICY(object_schema      => '%s',object_name        => '%s',policy_name        => '%s',statement_types    => '%s');END;",objectSchema, objectName, policyName, statement);
         return DataAccess.ResultOfExecuteSql(temp);
     }
     public static boolean DeleteAuditPolicy(String objectSchema, String objectName, String policyName)
